019dbfa147e199095e97a1f7362c91656fe1ae78
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2022-08-27 02:20:37 +0530

    Update King.sol

--- King.sol
+++ King.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
-contract Hack {
+contract King {
     constructor() public payable {
         address _to = 0x364A3497fa365b5c4b867289Ae523237469a85f6;
         (bool sent, bytes memory data) = _to.call{value: msg.value}("");
