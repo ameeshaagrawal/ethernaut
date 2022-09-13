adefa081ef1b0e9a70b431a51f5d8f71edd1f97f
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2022-09-13 10:42:23 +0530

    Update Privacy.js

--- Privacy.js
+++ Privacy.js
@@ -1,14 +1,14 @@
-const { ethers } = require("hardhat");
+const ethers = require("ethers");
 
 const main = async () => {
   try {
-    let contractEnsName = '0xc3AF3f37ABC2A6bF2FB4241CFC52207ddf5D43D8';
-    let storageAtSlot5 = await ethers.provider.getStorageAt(contractEnsName, 5);
+    let contractAddr = '0xc3AF3f37ABC2A6bF2FB4241CFC52207ddf5D43D8';
+    let storageAtSlot5 = await ethers.provider.getStorageAt(contractAddr, 5);
 
     // include 0x + 32 chars
     console.log(storageAtSlot5.substring(0,34)) 
     
-    // use the output as input in unlock()
+    // use the log output as input in unlock()
     
   } catch (error) {
     console.log("Error", error);
@@ -30,12 +30,17 @@ Smart contract:
 pragma solidity ^0.6.0;
 
 contract Privacy {
-
+  // slot 0
   bool public locked = true;
+  //slot 1
   uint256 public ID = block.timestamp;
+  
+  //slot 2 (8+8+16)
   uint8 private flattening = 10;
   uint8 private denomination = 255;
   uint16 private awkwardness = uint16(now);
+  
+  // slot 3, 4, 5
   bytes32[3] private data;
 
   constructor(bytes32[3] memory _data) public {
